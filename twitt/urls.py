7808e8a8656caaf76a17d00da8d3c05c0924bf26
shemanto27 <shemantosharkarofficial@gmail.com>
2024-08-27 01:34:32 +0600

    user pages added

--- twitt/urls.py
+++ twitt/urls.py
@@ -16,7 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from users.views import *
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('dashboard/', dashboard, name='dashboard'),
+    
 ]
